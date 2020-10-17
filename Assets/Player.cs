3294de89e58a43124133b4356cb5b9a7739e07aa
Damian Malczewski <damian.m.malczewski@gmail.com>
2020-10-17 14:27:04 +0200

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 position;
+
+    public float movement = 4.0f;
+    public float speed = 50.0f;
+
+    private int positionIndex = 0;
+
+    public void Start()
+    {
+        position = transform.position;
+    }
+
+    public void Update()
+    {
+        MoveTowardsPosition();
+        if (ShouldMoveUp())
+        {
+            MoveUp();
+        }
+        else if (ShouldMoveDown())
+        {
+            MoveDown();
+        }
+    }
+
+    private void MoveTowardsPosition() { transform.position = Vector2.MoveTowards(transform.position, position, speed * Time.deltaTime); }
+
+    private bool ShouldMoveUp() { return Input.GetKeyDown(KeyCode.UpArrow) && positionIndex < 1; }
+
+    private bool ShouldMoveDown() { return Input.GetKeyDown(KeyCode.DownArrow) && positionIndex > -1; }
+
+    private void MoveUp()
+    {
+        ++positionIndex;
+        position = new Vector2(transform.position.x, transform.position.y + movement);
+    }
+
+    private void MoveDown()
+    {
+        --positionIndex;
+        position = new Vector2(transform.position.x, transform.position.y - movement);
+    }
+}
