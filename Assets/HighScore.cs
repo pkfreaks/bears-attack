2b0142ce3f05be0c2880f1d5428cfc6665db5a42
Damianno <34073752+Damiarno@users.noreply.github.com>
2020-11-08 18:04:31 +0100

    Added basic display of results in the ranking

---
+++ Assets/HighScore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+class HighScore
+{ 
+    public int Score { get; set; }
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public string Date { get; set; }
+
+    public HighScore(int id, int score, string name, string date)
+    {
+        this.Score = score;
+        this.Name = name;
+        this.ID = id;
+        this.Date = date;
+    }
+}
