55a646be9e7521e8a124f355f25e722a5829e277
Bartosz Pawełczyk <bartek.pawelczyk1@wp.pl>
2020-11-26 19:55:10 +0100

    Added new player assets

---
+++ Assets/Models/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+public class Character : IComparable<Character>
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public bool isUnlocked { get; set; }
+    public bool isActive { get; set; }
+    public int price { get; set; }
+
+    public Character(int id, string name, int isUnlocked, int isActive, int price)
+    {
+        this.ID = id;
+        this.Name = name;
+        this.isUnlocked = Convert.ToBoolean(isUnlocked);
+        this.isActive = Convert.ToBoolean(isActive);
+        this.price = price;
+    }
+
+    public int CompareTo(Character other)
+    {
+        if (other.price < this.price)
+        {
+            return -1;
+        }
+        else if (other.price > this.price)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}
--- Assets/dbConn.cs
+++ Assets/dbConn.cs
@@ -5,13 +5,18 @@ using Mono.Data.Sqlite;
 using System.Data;
 using System;
 using UnityEngine.UI;
+using System.IO;
 
 public class dbConn : MonoBehaviour
 {
     private string conn;
 
     private List<HighScore> highScores = new List<HighScore>();
+    private List<Character> fetchedCharacters = new List<Character>();
+    private Character currentCharacter;
 
+    public RawImage playerModel;
+    public RawImage previewPlayerModel;
     public GameObject scorePrefab;
 
     public Transform scoreParent;
@@ -25,6 +30,26 @@ public class dbConn : MonoBehaviour
         conn = "URI=file:" + Application.dataPath + "/db/db.db";
 
         ShowScores();
+        fetchCharacters();
+        setCurrentActiveCharacter(currentCharacter);
+    }
+
+    public void LoadPreviousAsset()
+    {
+        var index = fetchedCharacters.IndexOf(currentCharacter) + 1;
+        index = index < 0 ? 0 : index;
+
+        var character = fetchedCharacters[index];
+        setCurrentActiveCharacter(character);
+    }
+
+    public void LoadNextAsset()
+    {
+        var index = fetchedCharacters.IndexOf(currentCharacter) + 1;
+        index = index > fetchedCharacters.Count - 1 ? fetchedCharacters.Count - 1 : index;
+
+        var character = fetchedCharacters[index];
+        setCurrentActiveCharacter(character);
     }
 
     private void ReadScores() 
@@ -68,6 +93,59 @@ public class dbConn : MonoBehaviour
                 scoreView.GetComponent<HighScoreManager>().SetScore(currentScore.Name, currentScore.Score.ToString(), "#" + (i + 1).ToString());
 
                 scoreView.transform.SetParent(scoreParent);
+            }
+        }
+    }
+
+    private void fetchCharacters()
+    {
+        using (IDbConnection dbconn = new SqliteConnection(conn))
+        {
+            dbconn.Open();
+
+            using (IDbCommand dbcmd = dbconn.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM Character";
+                dbcmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var character = new Character(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetInt32(3));
+
+                        if (character.isActive && character.isUnlocked) {
+                            currentCharacter = character;
+                        }
+
+                        fetchedCharacters.Add(character);
+                    }
+                    dbconn.Close();
+                    reader.Close();
+                }
+            }
+        }
+    }
+
+    private void setCurrentActiveCharacter(Character character)
+    {
+        var filePath = Application.dataPath + "Assets/Images/Player models/" + character.Name + ".png";
+
+        if (File.Exists(filePath))
+        {
+            Texture2D tex2D;
+            byte[] FileData;
+
+            FileData = File.ReadAllBytes(filePath);
+            tex2D = new Texture2D(2, 2);
+
+            if (tex2D.LoadImage(FileData))
+            {
+                playerModel = GameObject.Find("Main Player Image").GetComponent<RawImage>();
+                previewPlayerModel = GameObject.Find("Preview Player Image").GetComponent<RawImage>();
+
+                playerModel.texture = tex2D;
+                previewPlayerModel.texture = tex2D;
             }
         }
     }
